f7d94360452bf0c894d3b074a26a64d59185c8a2
David Dokupil <112353337+ddokupil12@users.noreply.github.com>
2022-10-31 12:11:11 -0500

    Update Gameboard_ideas.hs

---
+++ Gameboard_ideas.hs
@@ -0,0 +1,14 @@
+import Debug.Trace
+import Data.Maybe
+import Data.Record
+
+data Board = undefined
+{-
+Store1 Store 
+Holes1 Holes 
+Store2 Store  
+Holes2 Holes 
+-}
+
+type Store = Int
+type Holes = [Int]
