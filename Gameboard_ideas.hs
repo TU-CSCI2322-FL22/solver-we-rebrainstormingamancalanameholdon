005886720e0f241e65bc0be3e3241c5e358e73f7
David Dokupil <ddokupil@trinity.edu>
2022-11-02 12:05:27 -0500

    ideas

--- Gameboard_ideas.hs
+++ Gameboard_ideas.hs
@@ -3,9 +3,9 @@ import Data.Maybe
 --import Data.Record
 
 data Board = Board  {store1 :: Int
-                    , holes1 :: [Int]
+                    , holes1 :: [Hole]
                     , store2 :: Int
-                    , holes2 :: Int} deriving Show
+                    , holes2 :: [Hole]} deriving Show
 {-
 Store1 Store 
 Holes1 Holes 
@@ -14,16 +14,17 @@ Holes2 Holes
 -}
 
 type Store = Int
-type Holes = [Int]
+type Hole = (Int, [Int])
 --type Move = undefined
 type End = Bool
 --type Turn = Bool
 -- data Turn = Player1 | Player2 | Endgame
 
-checkEnd :: Board -> Bool
-checkEnd board = 
-    let
-        aux lst = 
-            let nonEmpties = [x | x <- lst, x /= []]
-            in nonEmpties != []
-    in aux holes1 || aux holes2
+-- checkEnd :: Board -> Bool -- Check if the game is over
+-- checkEnd board = 
+--     let
+--         aux lst = 
+--             let nonEmpties = [x | x <- lst, x /= []]
+--             in nonEmpties /= []
+--     in aux holes1 || aux holes2
+
