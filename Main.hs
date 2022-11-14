34471854829412b8051d5325b1cb412401808959
2021-08-24 <leldridg@trinity.edu>
2022-11-14 15:45:30 -0600

    completed an ugly version of readGame

--- Main.hs
+++ Main.hs
@@ -5,8 +5,9 @@ import Solver
 import Data.List.Split
 
 main :: IO ()
-main = do 
-    putStrLn "Hello, world!"
+main = do
+    putStrLn "Hello, World!"
+    --readGame "1\n0\n4 4 4 4 4 4\n0\n4 4 4 4 4 4"
 
 
 -- module things
@@ -37,9 +38,10 @@ getHoles str labels =
 getStore :: String -> Store
 getStore str = (read str :: Store)
 
---readGame :: String -> GameState
---readGame inputGS =  
---    let stringBoard  = lines inputGS
+readGame :: String -> GameState
+readGame inputGS =  
+    let playerLine:s1Line:h1Line:s2Line:h2Line:[] = lines inputGS
+    in  (getPlayer playerLine, Board (getStore s1Line) (getHoles h1Line [1..6]) (getStore s2Line) (getHoles h2Line [7..12]))
 --change to take a list of strings as input for helper functions
     
     
